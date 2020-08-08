4d638df5182b2cbc0ed3d7245fb715564dd3e593
rohmad-khoir <l200180101@student.ums.ac.id>
2020-08-08 14:43:22 +0700

    cb

--- resources/views/admin/about.blade.php
+++ resources/views/admin/about.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
       <div class="col-lg-3 col-6">
 
-        
+        <h1>about me</h1>
         <p>Nama : Rohmad Khoirudin</p>
         <p>Nim : l200180101</p>
-        <h1>about me</h1>
+        
         
 
       </div>
