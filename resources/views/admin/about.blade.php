a9b801baf176000608be1f2c41e89487a92c81a2
L200180101 <rohmadkhoirudin12@gmail.com>
2020-08-08 12:38:47 +0700

    add about

---
+++ resources/views/admin/about.blade.php
@@ -0,0 +1,11 @@
+@extends('home')
+@section('post_blog')
+<div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+      <div class="col-lg-3 col-6">
+        <h1>about me</h1>
+      </div>
+    </div>
+  </div>
+@endsection
